62ccbf7e9dda971ab330c52bd054212a4dda6b3c
chean.wei.khor <chean.wei.khor@gmail.com>
2023-04-23 22:44:40 +0800

    logout confirmation

--- fronted/src/pages/common/LogoutConfirmation.jsx
+++ fronted/src/pages/common/LogoutConfirmation.jsx
@@ -1,24 +1,25 @@
 import axios from "axios";
-import { Link, Navigate, useParams, useNavigate  } from 'react-router-dom';
-import {useContext, useState, useEffect } from 'react';
+import { Link, Navigate, useParams, useNavigate } from 'react-router-dom';
+import { useContext, useState, useEffect } from 'react';
 
 import { UserContext } from './../context/UserContext';
 
-export default function LogoutConfirmation({onClose}) {
+export default function LogoutConfirmation({ onClose }) {
     const navigate = useNavigate();
 
-    const {contextAgentUsername, setContextAgentUsername, isLogoutConfirmation, setIsLogoutConfirmation} = useContext(UserContext);
-    
+    const { contextAgentUsername, setContextAgentUsername, isLogoutConfirmation, setIsLogoutConfirmation } = useContext(UserContext);
+
     async function logOut() {
         try {
-            await axios.post("agent_account/logout");
+            /*await axios.post("agent_account/logout");*/
             setContextAgentUsername(null);
             navigate('/login')
             setIsLogoutConfirmation(!isLogoutConfirmation);
             alert("Log out successfully");
         } catch (error) {
+            console.log(error);
             alert("Log out failed.");
-        }   
+        }
     }
 
     return (
@@ -31,15 +32,15 @@ export default function LogoutConfirmation({onClose}) {
                     type="button"
                     className="inline-block rounded bg-white px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-teal-300-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-teal-300-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-teal-300-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
                     onClick={onClose}
-                    >
+                >
                     CANCEL
                 </button>
                 <button
-                type="button"
-                className="inline-block rounded bg-teal-300 px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-teal-300-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-teal-300-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-teal-300-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-                onClick={logOut}
+                    type="button"
+                    className="inline-block rounded bg-teal-300 px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-teal-300-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-teal-300-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-teal-300-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                    onClick={logOut}
                 >
-                Log Out
+                    Log Out
                 </button>
             </div>
         </form>
