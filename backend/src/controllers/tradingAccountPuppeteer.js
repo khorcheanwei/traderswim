c2221d28c4202ee8b8d8c179b03b1d80b8798fc7
weikhor <cheanwei.KCW@gmail.com>
2023-06-19 23:29:48 +0800

    button

--- backend/src/controllers/tradingAccountPuppeteer.js
+++ backend/src/controllers/tradingAccountPuppeteer.js
@@ -242,8 +242,10 @@ async function puppeteer_login_account(agentID, accountUsername, accountPassword
 
                     console.log('Login thinkorswim website...........................')
 
-                    await page.waitForSelector('[id="accept"]');
-                    await page.click('[id="accept"]');
+                    await Promise.all([
+                        page.waitForSelector('[id="accept"]'),
+                        page.click('[id="accept"]')
+                    ]);
 
                     await page.waitForSelector('[data-testid="home-page"]', { timeout: 15000 });
                     console.log('Login successfully');
