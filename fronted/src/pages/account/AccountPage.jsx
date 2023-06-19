d9f46d5ddb65a032dc2a1253f87c99e86abdfdfc
weikhor <cheanwei.KCW@gmail.com>
2023-06-20 00:00:58 +0800

    account

--- fronted/src/pages/account/AccountPage.jsx
+++ fronted/src/pages/account/AccountPage.jsx
@@ -59,7 +59,7 @@ export default function AccountsPage() {
 
   const ref = useRef(null)
   useEffect(() => {
-    ref.current = setInterval(fetchAccountData, 1 * 500);
+    ref.current = setInterval(fetchAccountData, 1 * 1500);
     return () => {
       if(ref.current){
         clearInterval(ref.current);
