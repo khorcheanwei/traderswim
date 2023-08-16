fc06d3b1285331c840d88005e1ede94b243de334
weikhor <cheanwei.KCW@gmail.com>
2023-08-17 01:21:56 +0800

    add

--- fronted/src/pages/account/AccountTable.jsx
+++ fronted/src/pages/account/AccountTable.jsx
@@ -28,21 +28,6 @@ function GlobalFilter({
   return (
     <div className="w-full">
       <div className="flex justify-end">
-        {/*
-        <label className="flex gap-x-2 items-baseline">
-          <span className="text-gray-700">Search: </span>
-          <input
-            type="text"
-            className="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            value={value || ""}
-            onChange={e => {
-              setValue(e.target.value);
-              onChange(e.target.value);
-            }}
-            placeholder={`${count} records...`}
-          />
-        </label>
-          */}
         <Button className="text-gray-700 " onClick={accountAddClose}>ADD ACCOUNT</Button>
       </div>
       <div>
