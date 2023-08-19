e803d3d34da3ba8505ad44e1d35cf5781ae78ec8
Khor Chean Wei <cheanwei.KCW@gmail.com>
2023-08-19 23:20:58 +0800

    Place order change order

--- backend/src/cronStockCopyTradingProcess.js
+++ backend/src/cronStockCopyTradingProcess.js
@@ -37,32 +37,6 @@ cron.schedule('*/2 * * * * *', async () => {
   try {
     for (let index = 0; index < agent_list.length; index++) {
       const agentID = agent_list[index];
-      // account database by agent
-      waitForResult(async () => await account_database_by_agent(agentID), 2000)
-        .then((result) => {
-          console.log(result); 
-        })
-        .catch((error) => {
-          console.log(error); 
-        });
-
-      // copy trading database by agent
-      waitForResult(async () => await copy_trading_database_by_agent(agentID), 2000)
-        .then((result) => {
-          console.log(result); 
-        })
-        .catch((error) => {
-          console.log(error); 
-        });
-
-      // copy trading position by agent
-      waitForResult(async () => await copy_trading_position_by_agent(agentID), 2000)
-        .then((result) => {
-          console.log(result); 
-        })
-        .catch((error) => {
-          console.log(error); 
-        });
       
       // stock copy trading database by agent
       waitForResult(async () => await stock_copy_trading_database_by_agent(agentID), 2000)
--- fronted/src/pages/stockTrading/stockPlaceOrderPanel/StockPlaceSaveOrderPanelTable.jsx
+++ fronted/src/pages/stockTrading/stockPlaceOrderPanel/StockPlaceSaveOrderPanelTable.jsx
@@ -17,36 +17,31 @@ function GlobalFilter({
 }
 
 export function SettingsPanel({row, setStockSaveOrderList}) {
-  let optionChainSymbol = row.cell.row.original.optionChainSymbol;
+  let stockSymbol = row.cell.row.original.stockSymbol;
 
-  const optionContractSaveOrderDeleteClose = async () => {
-    const {data} = await axios.delete("/option_contract_save_order/remove_option_contract_save_order/", { data:{ "optionChainSymbol": optionChainSymbol }});
+  const stockSaveOrderDeleteClose = async () => {
+    const {data} = await axios.delete("/stock_save_order/remove_stock_save_order/", { data:{ "stockSymbol": stockSymbol }});
     if (data.success != true) {
-        alert("Remove option contract save order failed");
+        alert("Remove stock save order failed");
     } else {
-        alert("Remove option contract save order successful");
+        alert("Remove stock save order successful");
         setStockSaveOrderList(data.list);
     }
-
-    /*
-    const list = [...optionContractTickerList];
-    list.splice(index, 1);
-    setOptionContractTickerList(list);*/
   };
   
 
   return (
     <div className="flex">
-      <svg onClick={optionContractSaveOrderDeleteClose} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-2 cursor-pointer">
+      <svg onClick={stockSaveOrderDeleteClose} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-2 cursor-pointer">
         <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
       </svg>
     </div>
   );
 }
 
-function StockOptionPlaceSaveOrderPanelTable({ columns, data }) {
+function StockPlaceSaveOrderPanelTable({ columns, data }) {
   let hiddenColumns = [];
   return CommonTable({ columns, data, GlobalFilter, hiddenColumns })
 }
 
-export default StockOptionPlaceSaveOrderPanelTable;
+export default StockPlaceSaveOrderPanelTable;
