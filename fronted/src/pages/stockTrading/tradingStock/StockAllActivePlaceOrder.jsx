1c44aeff5ffb7dcac566a1115954618521912ee7
weikhor <cheanwei.KCW@gmail.com>
2023-08-26 16:00:10 +0800

    add

--- fronted/src/pages/stockTrading/tradingStock/StockAllActivePlaceOrder.jsx
+++ fronted/src/pages/stockTrading/tradingStock/StockAllActivePlaceOrder.jsx
@@ -148,7 +148,7 @@ export default function StockAllActivePlaceOrder({ onClose }) {
     }
 
 
-    async function handleOrder() {
+    async function handlePlaceOrder() {
         setDisabledButton(true);
         try {
             const allTradingAccountsOrderList = [];
@@ -283,7 +283,7 @@ export default function StockAllActivePlaceOrder({ onClose }) {
                                 <button
                                     type="button"
                                     className="inline-block rounded bg-teal-300 px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-teal-300-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-teal-300-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-teal-300-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-                                    onClick={handleOrder}
+                                    onClick={handlePlaceOrder}
                                     disabled={disabledButton}>
                                     Place order
                                 </button>
