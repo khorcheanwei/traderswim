f1d006ae70ab3f1e7a6ff1ab25291f70f9ce8abc
Khor Chean Wei <cheanwei.KCW@gmail.com>
2023-08-31 18:43:48 +0800

    add (#58)

--- fronted/src/pages/stockTrading/tradingStock/StockReplaceOrderIndividual.jsx
+++ fronted/src/pages/stockTrading/tradingStock/StockReplaceOrderIndividual.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useState, memo } from 'react';
-import StockHandleOrder, {get_duration_and_session} from './StockHandleOrder';
+import StockHandleOrder, {get_duration_and_session, get_duration_and_session_reverse} from './StockHandleOrder';
 import { ClipLoader } from 'react-spinners';
 
 const StockReplaceOrderIndividual = memo(({ rowCopyTradingOrderIndividual, onClose }) => {
@@ -44,20 +44,6 @@ const StockReplaceOrderIndividual = memo(({ rowCopyTradingOrderIndividual, onClo
     const [stockStopPriceOffset, setStockStopPriceOffset] = useState(rowStockStopPriceOffset);
 
     const [disabledButton, setDisabledButton] = useState(false);
-
-    function get_duration_and_session_reverse(session, duration) { 
-        if(session == "NORMAL" && duration == "DAY") {
-            return "DAY";
-        } else if (session == "NORMAL" && duration == "GOOD_TILL_CANCEL") {
-            return "GTC";
-        } else if (session == "SEAMLESS" && duration == "DAY") {
-            return "EXT"
-        } else if (session == "SEAMLESS" && duration == "GOOD_TILL_CANCEL") {
-            return "GTC_EXT"
-        } else {
-            return null;
-        }
-    }
 
     async function handleReplaceOrderIndividual() {
         // replace order
