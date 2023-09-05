8caa80f761b8535d84b0b83eb8f8425e21210f0f
Khor Chean Wei <cheanwei.KCW@gmail.com>
2023-09-05 21:20:26 +0800

    Stock sell enhance (#64)
    * add
    
    * add
    
    * add
    
    * add
    
    * add

--- fronted/src/pages/stockTrading/copyTradingPosition/StockCopyTradingAllAccountPositionPage.jsx
+++ fronted/src/pages/stockTrading/copyTradingPosition/StockCopyTradingAllAccountPositionPage.jsx
@@ -1,12 +1,17 @@
-import axios from 'axios';
+import Overlay from "../../../pages/Overlay"
 import React from 'react'
 import { useContext, useState, useEffect, useRef  } from 'react';
 
 import { StockCopyTradingPositionContext } from '../context/StockCopyTradingPositionContext';
-import StockCopyTradingAllAccountPositionTable from './StockCopyTradingAllAccountPositionTable'
-import {  SettledQuantityColorChange } from './StockCopyTradingPositionTable'
+import StockCopyTradingAllAccountPositionTable from './StockCopyTradingAllAccountPositionTable';
+import {  SettledQuantityColorChange } from './StockCopyTradingPositionTable';
+import { MakeSelectedStockPositionPanel }  from './StockCopyTradingAllAccountPositionTable';
+import StockWarningMessage from './../tradingStock/StockWarningMessage';
+import StockExitOrderSelected from '../tradingStock/StockExitOrderSelected';
 
 export default function StockCopyTradingAllAccountPositionPage({ rowCopyTradingPosition, onClose }) {
+
+  const [selectedPositionDict, setSelectedPositionDict] = useState({});
 
   const columns = React.useMemo(() => [
     {
@@ -33,25 +38,72 @@ export default function StockCopyTradingAllAccountPositionPage({ rowCopyTradingP
     {
       Header: 'Account Username',
       accessor: 'accountUsername',
+    },
+    {
+      Header: 'SELECTED POSITION',
+      accessor: 'makeSelectedPosition',
+      Cell: (row) => <MakeSelectedStockPositionPanel row={row} setSelectedPositionDict={setSelectedPositionDict}/>,
     }
   ], [])
 
   const stockSymbol = rowCopyTradingPosition.row.original.stockSymbol;
+
+  const [isOpenPositionExitSelected, setIsOpenPositionExitSelected] = useState(false);
+  const [warningMessage, setWarningMessage] = useState("");
+  const [isOpenWarningMessagePositionSelected, setIsOpenWarningMessagePositionSelected] = useState(false); 
 
   const {stockCopyTradingPositionDataDict, setStockCopyTradingPositionDataDict} = useContext(StockCopyTradingPositionContext);
   const stockCopyTradingPositionAllAccountData = stockCopyTradingPositionDataDict[stockSymbol];
 
   var data = React.useMemo(() => stockCopyTradingPositionAllAccountData, [stockCopyTradingPositionAllAccountData])
 
+  const warningMessagePositionSelectedClose = async () => {
+    setIsOpenWarningMessagePositionSelected(!isOpenWarningMessagePositionSelected);
+  }
+
+  const positionExitSelectedClose = async () => {
+    if (Object.keys(selectedPositionDict).length == 0) {
+      setWarningMessage("Please select at least one position!");
+      warningMessagePositionSelectedClose();
+    } else {
+      const stockSettledQuantitySet = new Set();
+
+      for (const accountUsername in selectedPositionDict) {
+        const selectedPosition = selectedPositionDict[accountUsername];
+        stockSettledQuantitySet.add(selectedPosition["stockSettledQuantity"]);
+      }
+
+      if (stockSettledQuantitySet.size > 1) {
+        setWarningMessage("Quantity size of position selected are not same!");
+        warningMessagePositionSelectedClose();
+      } else {
+        setIsOpenPositionExitSelected(!isOpenPositionExitSelected);
+      }
+    }
+  }
+
   return (
     <div>
-        <button
-            type="button"
-            className="inline-block rounded bg-white px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-teal-300-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-teal-300-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-teal-300-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-            onClick={onClose}>
-            CANCEL
-        </button>
-        <StockCopyTradingAllAccountPositionTable columns={columns} data={data} />
+        <div className="flex justify-between">
+          <button
+              type="button"
+              className="inline-block rounded bg-white px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-teal-300-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-teal-300-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-teal-300-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+              onClick={onClose}>
+              CANCEL
+          </button>
+          <div className="flex space-x-2 mr-10">
+            <div onClick={positionExitSelectedClose} className="cursor-pointer relative inline-flex items-center justify-center w-10 h-10 overflow-hidden rounded-full bg-red-600">
+              <span className="font-medium text-white dark:text-white">S</span>
+            </div>
+        </div>
+      </div>
+      <Overlay isOpen={isOpenWarningMessagePositionSelected} >
+        <StockWarningMessage warningMessage={warningMessage} onClose={warningMessagePositionSelectedClose}></StockWarningMessage>
+      </Overlay>
+      <Overlay isOpen={isOpenPositionExitSelected} >
+        <StockExitOrderSelected selectedPositionDict={selectedPositionDict} onClose={positionExitSelectedClose}></StockExitOrderSelected>
+      </Overlay>
+      <StockCopyTradingAllAccountPositionTable columns={columns} data={data} />
     </div>
   );
 }
---
+++ fronted/src/pages/stockTrading/tradingStock/StockExitOrderSelected.jsx
@@ -0,0 +1,105 @@
+import axios from 'axios';
+import { useState } from 'react';
+import StockHandleOrder, {get_duration_and_session} from './StockHandleOrder';
+
+export default function StockExitOrderSelected({ selectedPositionDict, onClose }) {
+    
+    const [isLoading, setIsLoading] = useState(false);
+
+    var stockInstructionList = ["BUY", "SELL"];
+    var stockOrderTypeList = ["MARKET", "LIMIT", "STOP", "STOP_LIMIT", "TRAILING_STOP"];
+    var stockSessionDurationList = ["DAY", "GTC", "EXT", "GTC_EXT"];
+
+    var stockStopPriceLinkTypeDict = {"$ Dollars": "VALUE", "% Percent":"PERCENT"};
+    var stockStopPriceLinkTypeReverseDict = {"VALUE": "$ Dollars", "PERCENT":"% Percent"};
+
+    let firstAccountUsername = Object.keys(selectedPositionDict)[0];
+    let firstSelectedPosition = selectedPositionDict[firstAccountUsername];
+
+    let rowStockSymbol = firstSelectedPosition.stockSymbol;
+    let rowStockPrice = firstSelectedPosition.stockAveragePrice;
+    let rowStockQuantity = firstSelectedPosition.stockSettledQuantity;
+    
+    const [stockSymbol, setStockSymbol]= useState(rowStockSymbol);
+    const [stockInstruction, setStockInstruction] = useState("SELL");
+    const [stockSessionDuration, setStockSessionDuration] = useState("DAY");
+    const [stockOrderType, setStockOrderType] = useState("LIMIT");
+    const [stockQuantity, setStockQuantity] = useState(rowStockQuantity);
+
+    const [stockPrice, setStockPrice] = useState(rowStockPrice);
+    const [stockStopPrice, setStockStopPrice] = useState(0);
+    const [stockStopPriceLinkTypeSymbol, setStockStopPriceLinkTypeSymbol] = useState("$ Dollars");
+    const [stockStopPriceOffset, setStockStopPriceOffset] = useState(0.1);
+
+    const [disabledButton, setDisabledButton] = useState(false);
+
+    async function handleExitOrder() {
+        setDisabledButton(true);
+        try {
+            let allTradingAccountsOrderList = [];
+
+            for (const accountUsername in selectedPositionDict) {
+                const selectedPosition = selectedPositionDict[accountUsername];
+                const selectedPositionAccountData =  {
+                    accountId: selectedPosition["accountId"],
+                    accountName: selectedPosition["accountName"],
+                    accountUsername: selectedPosition["accountUsername"]
+                }
+                allTradingAccountsOrderList.push(selectedPositionAccountData);
+            }
+
+            const {stockSession, stockDuration} = get_duration_and_session(stockSessionDuration);
+            const stockStopPriceLinkType = stockStopPriceLinkTypeDict[stockStopPriceLinkTypeSymbol];
+
+            const { data } = await axios.post("/stock_copy_trading/exit_order/", { allTradingAccountsOrderList, stockSymbol, stockSession, stockDuration, 
+                stockInstruction, stockOrderType, stockQuantity, stockPrice, stockStopPrice, stockStopPriceLinkType, stockStopPriceOffset })
+
+            if (data != "success") {
+                alert("Exit order failed");
+            } else {
+                alert("Exit order successful");
+                onClose();
+            }
+        } catch (error) {
+            alert("Exit order failed")
+            console.log(error.message);
+        }
+        setDisabledButton(false);
+    }
+
+    return (
+        <div>
+            <div className="mb-4">
+                <h1 className="block text-gray-700 text-lm font-bold mb-2">Stock Exit Order</h1>
+            </div>
+            <StockHandleOrder 
+                isGetStockQuotes={false}
+                isLoading={isLoading} setIsLoading={setIsLoading} 
+                stockSymbol={stockSymbol} setStockSymbol={setStockSymbol}
+                stockInstruction={stockInstruction} setStockInstruction={setStockInstruction}
+                stockSessionDuration={stockSessionDuration} setStockSessionDuration={setStockSessionDuration}
+                stockOrderType={stockOrderType} setStockOrderType={setStockOrderType}
+                stockQuantity={stockQuantity} setStockQuantity={setStockQuantity}
+                stockPrice={stockPrice} setStockPrice={setStockPrice}
+                stockStopPrice={stockStopPrice} setStockStopPrice={setStockStopPrice}
+                stockStopPriceLinkTypeSymbol={stockStopPriceLinkTypeSymbol} setStockStopPriceLinkTypeSymbol={setStockStopPriceLinkTypeSymbol}
+                stockStopPriceOffset={stockStopPriceOffset} setStockStopPriceOffset={setStockStopPriceOffset}>    
+            </StockHandleOrder>
+            <div className="flex justify-end gap-5">
+                <button
+                    type="button"
+                    className="inline-block rounded bg-white px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-teal-300-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-teal-300-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-teal-300-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                    onClick={onClose}>
+                    CANCEL
+                </button>
+                <button
+                    type="button"
+                    className="inline-block rounded bg-teal-300 px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-teal-300-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-teal-300-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-teal-300-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                    onClick={handleExitOrder}
+                    disabled={disabledButton}>
+                    Exit order
+                </button>
+            </div>
+        </div>
+    )
+}
